c059bd19e6946e88bd1a21ae1feee1f4ab871b2b
Jiangarang <55816618+Jiangarang@users.noreply.github.com>
2022-03-24 19:12:57 -0400

    added pylint to vscode. Pls don't delete

--- transactions.py
+++ transactions.py
@@ -1,11 +1,14 @@
+import sqlite3
+import csv
 '''
-in a new file transactions.py which will store financial 
-transactions with the fields. It should have an __init__ method 
-where you pass in the filename for the database to be used 
-(e.g. tracker.db) and each transaction should have the following 
-fields stored in a SQL table called transactions. 
+create a Python class Transaction in a new file transactions.py 
+which will store financial transactions with the fields. 
+It should have an __init__ method where you pass in the filename 
+for the database to be used (e.g. tracker.db) and each transaction 
+should have the following fields stored in a SQL table called transactions. 
 The transaction class should not do any printing!! 
 'item #','amount','category','date','description'
+
 
 '''
 
